fc3488120ef672014ac3d48afbf5b192aba19591
Rachel S Kim / 김민정 <106638262+loafcheck@users.noreply.github.com>
2022-11-22 16:08:07 +0900

    Update and rename about link to linkExplanation.js

---
+++ process/linkExplanation.js
@@ -0,0 +1,17 @@
+![Screen Shot 2022-11-22 at 3 55 04 PM](https://user-images.githubusercontent.com/106638262/203246869-b933514e-3bb1-4b74-ab53-acbf42e8ec08.png)
+https://github.com/loafcheck/online_store-react_practice/issues/2#issue-1459246805
+<img alt="Coding" src="https://media.giphy.com/media/eg4q8ka6zQuQ2qgKwe/giphy.gif" align="right"/>
+
+// we can specify the links in react with three different ways
+// 1. purf html reference
+// 2. link
+// 3. nav.link
+
+// pure html reference is a typical anchor link element, 
+// so when we click the link the whole application is reloaded from the beginning 
+// so it is no effective in react
+
+// link and nav.link are similar in React.
+// nav.link gives us a couple of additional things.
+// we can add in here active class name and 
+// that will automatically tie your css class to current url 
