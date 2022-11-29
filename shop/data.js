ce996c68e34e0e7f0e411c35ca8d9f4f674d6961
Rachel S Kim / 김민정 <106638262+loafcheck@users.noreply.github.com>
2022-11-29 13:33:56 +0900

    Create data.js

---
+++ shop/data.js
@@ -0,0 +1,37 @@
+[
+ {
+        id : 0,
+        title : "Stain Lamp",
+        content : "Made in France",
+        price : "12,000,000 won" 
+    },
+
+    {
+        id : 1,
+        title : "Neo Bar Chair",
+        content : "Made in France",
+        price : "7,000,000 won "
+    },
+
+    {
+        id : 2,
+        title : "Guard Vase",
+        content : "Made in France",
+        price : "1,300,000 won" 
+    },
+
+    {
+        id : 3,
+        title : "Wood Desk",
+        content : "Made in France",
+        price : "2,300,000 won" 
+    },
+
+    {
+        id : 4,
+        title : "Silver Lamp",
+        content : "Made in France",
+        price : "800,000 won"
+    },
+
+]
